7d28c4f2bfd461a5c970dbff90998a6fed41333d
codewithgaurav-tech <glalwani177@gmail.com>
2023-10-14 21:40:50 +0530

    'improve_error_handling'

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,5 @@
 // src/users/users.controller.ts
-import { Controller, Post, Body,Get } from '@nestjs/common';
+import { Controller, Post, Body, Get } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UserDTO } from './dto/user-registration.dto';
 
@@ -9,11 +9,23 @@ export class UsersController {
 
   @Post('register')
   async registerUser(@Body() userDto: UserDTO) {
-    const newUser = await this.usersService.createUser(userDto);
-    return newUser;
+    try {
+      const newUser = await this.usersService.createUser(userDto);
+      return newUser;
+    } catch (error) {
+      // Handle the error and send it in the API response
+      return { error: error.message };
+    }
   }
+
   @Get()
   async GetUser() {
-    return this.usersService.GetUser();
+    try {
+      const users = await this.usersService.GetUser();
+      return users;
+    } catch (error) {
+      // Handle the error and send it in the API response
+      return { error: error.message };
+    }
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -7,13 +7,23 @@ import { UserDTO } from './dto/user-registration.dto';
 
 @Injectable()
 export class UsersService {
-    constructor(@InjectModel('User') private readonly userModel: Model<User>) { }
+  constructor(@InjectModel('User') private readonly userModel: Model<User>) {}
 
-    async createUser(userDto: UserDTO): Promise<User> {
-        const newUser = new this.userModel(userDto);
-        return newUser.save();
+  async createUser(userDto: UserDTO): Promise<User> {
+    try {
+      const newUser = new this.userModel(userDto);
+      return await newUser.save();
+    } catch (error) {
+      throw new Error("Failed to create a user: " + error.message);
     }
-    async GetUser() {
-        return this.userModel.find().exec();
+  }
+
+  async GetUser() {
+    try {
+      const users = await this.userModel.find().exec();
+      return users;
+    } catch (error) {
+      throw new Error("Failed to retrieve users: " + error.message);
     }
+  }
 }
