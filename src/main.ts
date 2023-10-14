34f099b9ef878f75e73f7e38a24f45d4ae7b8a37
codewithgaurav-tech <glalwani177@gmail.com>
2023-10-14 22:13:18 +0530

    'added_cors_origin'

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,12 @@ require('dotenv').config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Allow requests from any origin
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // If you need to include cookies in cross-origin requests
+  });
   await app.listen(4000);
 }
 bootstrap();
